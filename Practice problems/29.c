dd8e592d5ea2722339c0d9444d30bbc66cdb3626
Precise Goals <elitesarthakamr@gmail.com>
2024-11-26 03:00:18 +0530

    One problem solved today no time left :(

--- Practice problems/29.c
+++ Practice problems/29.c
@@ -1,27 +1,31 @@
-// #include <math.h>
-// #include <stdio.h>
-// int main()
-// {
-//     // Write a C program to check whether a given number is an Armstrong number or not.
-//     int num, og, r, dig = 0, sum = 0;
-//     printf("Enter the Number: ");
-//     scanf("%d", &num);
-//     og = num;
-//     while (num != 0) {
-//         num /= 10;
-//         dig++;
-//     }
-//     num = og;
-//     while (num != 0) {
-//         r = num % 10;
-//         sum += pow(r, dig);
-//         num /= 10;
-//     }
-//     if (og == sum) {
-//         printf("The Number is Armstrong\n");
-//     } else {
-//         printf("The Number is not Armstrong\n");
-//     }
+#include <math.h>
+#include <stdio.h>
+int main()
+{
+    // Write a C program to check whether a given number is an Armstrong number or not.
+    int num, og, r, dig = 0;
+    float sum = 0;
+    printf("Enter the Number: ");
+    scanf("%d", &num);
+    og = num;
+    while (num != 0) {
+        num /= 10;
+        dig++;
+    }
+    num = og;
+    // printf("%d\n", dig);
+    while (num != 0) {
+        r = num % 10;
+        // printf("%d\n", r);
+        sum += pow(r, dig);
+        // printf("%f\n", pow(r, dig));
+        num /= 10;
+    }
+    if (og == sum) {
+        printf("The Number is Armstrong\n");
+    } else {
+        printf("The Number is not Armstrong\n");
+    }
 
-//     return 0;
-// }
+    return 0;
+}
